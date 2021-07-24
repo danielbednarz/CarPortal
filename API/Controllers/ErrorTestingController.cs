c53ee298ee3a32001a8dcf30c32682cba9a81c69
Daniel Bednarz <daniosdb@gmail.com>
2021-07-24 11:39:42 +0200

    Konfiguracja gitignore

---
+++ API/Controllers/ErrorTestingController.cs
@@ -0,0 +1,53 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class ErrorTestingController : BaseController
+    {
+        private readonly DataContext _context;
+        public ErrorTestingController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [Authorize]
+        [HttpGet("auth")]
+        public ActionResult<string> GetSecret()
+        {
+            return "tajny tekst";
+        }
+
+        [HttpGet("not-found")]
+        public ActionResult<AppUser> GetNotFound()
+        {
+            var user = _context.Users.Find(-1);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+            else
+                return Ok(user);
+        }
+
+        [HttpGet("server-error")]
+        public ActionResult<string> GetServerError()
+        {
+            var uncreatedUser = _context.Users.Find(-34);
+
+            var status = uncreatedUser.ToString();
+
+            return status;
+        }
+
+        [HttpGet("bad-request")]
+        public ActionResult<string> GetBadRequest()
+        {
+            return BadRequest("Złe przekierowanie");
+        }
+
+    }
+}
